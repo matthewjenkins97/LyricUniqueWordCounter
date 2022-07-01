fe75026a8aaad3309f81f9b9b2d8dd34dd7a5f24
Matthew R. Jenkins <matthewjenkins97@gmail.com>
2022-07-01 19:41:09 -0400

    added documentation

--- main.py
+++ main.py
@@ -15,23 +15,25 @@ def get_albums(artist_name):
     # please substitute this with your own genius key if you are going to fork this.
     genius = lyricsgenius.Genius("GehPJjxd7tBLvvMTbDQnTb01EV00_Pt6Swzp6N2aunBWlVSZ8zlQzZ0GYugYmrnT")
     genius.remove_section_headers = True
+
+    # search for artist, get artist ID for searching artist's albums, search albums
     artist_search = genius.search_artists(artist_name)
     artist_id = artist_search["sections"][0]["hits"][0]["result"]["id"]
     albums = genius.artist_albums(artist_id)
     albums = albums["albums"]
 
-    # do we want to sort by creation dates?
-    # seems like there's a case where it can shit itself if you give it a none type.
+    # TODO: known issue where sometimes release_date_components > year is NoneType
+    # not sure how to bypass that offhand, may need to do more testing.
 
-    # print(albums)
-
-    # def sort_by_year(json):
-    #     try:
-    #         return json["release_date_components"]["year"]
-    #     except KeyError:
-    #         return 0
-    # albums.sort(key = sort_by_year)
+    # sort albums by year (earliest to latest)
+    def sort_by_year(json):
+        try:
+            return json["release_date_components"]["year"]
+        except KeyError:
+            return 0
+    albums.sort(key = sort_by_year)
     
+    # save each album's lyrics in a json file
     for album in albums:
         try:
             searched_album = genius.search_album(f"{album['name']} {artist_name}")
@@ -39,18 +41,25 @@ def get_albums(artist_name):
         except requests.Timeout:
             continue
 
-
 def generate_unique_word_count():
     """generates the unique word count by going through all the lyrics, pruning
     extra characters at the beginning and end of each lyric sheet,
     removing new lines, apostrophes, and non alphanumeric and space
     characters.
+
     we then split each word and use only the first 35000 words to calculate
     the unique strings."""
 
     buffer = ""
+
+    # get each file and sort it by creation time
     list_of_files = list(filter(os.path.isfile, os.listdir("./")))
     list_of_files.sort(key=os.path.getmtime)
+
+    # get each file that starts with Lyrics, load every track, removing the first section and last section
+    # and then add that to the buffer
+    # with lyricsgenius the first section starts like "Song Name Lyrics\n"
+    # the last section always seems to end with a random number followed by the word "Embed"
     for file in list_of_files:
         if file.startswith("Lyrics"):
             lyrics = open(file, "r")
@@ -61,6 +70,9 @@ def generate_unique_word_count():
                 lyrics = re.sub(r'\d+Embed', '', lyrics)
                 buffer += lyrics
 
+    # turn buffer lowercase, remove new lines, remove apostrophes, remove non alphanumeric and space characters,
+    # split each word, and use only the first 35000 words
+    # 35000 is used per the logic in https://pudding.cool/projects/vocabulary/index.html
     buffer = buffer.lower()
     buffer = re.sub('\n', ' ', buffer)
     buffer = re.sub('\'', '', buffer)
@@ -68,16 +80,17 @@ def generate_unique_word_count():
     buffer = buffer.split(' ')
     buffer = buffer[0:35000]
 
+    # get length of the set with all unique words
+    # return unique strings and diversity percentage (perhaps that's not needed)
     unique_strings = len(set(buffer))
-
     print(f"{unique_strings} unique strings")
     print(unique_strings / len(buffer))
 
     # TODO: remove all json files which start with Lyrics after we generate 
+    # Maybe not because we're optimally running this in a docker container?
 
 def main():
     """main method."""
-
     if len(sys.argv) == 2:
         get_albums(sys.argv[1])
         generate_unique_word_count()
